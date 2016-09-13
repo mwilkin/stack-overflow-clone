9234982b78714f45d541dd445db09a8ca04f174c
Dylan Barnard and Matt Wilkin <dylan+matt@>
2016-09-13 14:56:43 -0700

    Allow new users to log in to site

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,12 @@ class User < ActiveRecord::Base
   end
 
   def self.authenticate(email, password)
+    user = User.where(email: email).first
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :questions
   resources :users
-
-  get '/' => 'questions#index'
+  root :to => "questions#index"
+  # resources :sessions, :as => "log_in", :only => [:new, :create]
+  # resources :sessions, :only => [:new, :create]
+  get "/log-in" => "sessions#new"
+  post "/log-in" => "sessions#create"
+  get '/log-out' => 'sessions#destroy', as: :log_out
 end
